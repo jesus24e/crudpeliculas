ca195e0417b3966fa931ced247742609364c05ef
jesus24e <jesusrodriguez65@aragon.unam.mx>
2025-06-18 20:40:36 -0600

    primer commit

---
+++ index.js
@@ -0,0 +1,46 @@
+//Liberia que nos ayuda a gestionar la aplicación web
+const express = require("express");
+const app = express();
+
+//Lamamos al repositorio
+const {
+  agregar,
+  obtenerPorId,
+  obtenerTodos,
+} = require("./pelicula.repositorio.js");
+
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+
+app.get("/", async(req, res) => {
+  res.json({ mensaje: "Hola mundo" });
+});
+
+app.get('/api/peliculas', async(req, res) => {
+    let peliculas = await obtenerTodos()
+    res.status(200).json(peliculas)
+})
+
+app.get('/api/peliculas/:id', async (req, res) => {
+    //console.log(req.params.id)
+    let pelicula =await obtenerPorId(req.params.id)
+    if (pelicula == undefined)
+        return res.status(404).json({ mensaje: "Pelicula no encontrada con el id: " + req.params.id })
+    return res.status(200).json(pelicula)
+})
+app.post('/api/peliculas/', async(req, res) => {
+    console.log(req.body)
+    let pelicula = {
+        titulo: req.body.titulo,
+        visto: false,
+        resumen: req.body.resumen
+    }
+    let id = await agregar(pelicula)
+    //pelicula.id = id
+    res.status(201).json(pelicula)
+})
+
+app.listen(3000, () => {
+  console.log("http://localhost:3000");
+});
